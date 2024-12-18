3ce0046981f6f207f317124e829007eddb1161d6
avinashverma <avinashverma165@gmail.com>
2024-12-18 11:27:33 +0530

    added dummy data

---
+++ models/category.js
@@ -0,0 +1,10 @@
+// implemented in dummy-data.js
+class Category {
+	constructor(id, title, color) {
+		this.id = id;
+		this.title = title;
+		this.color = color;
+	}
+}
+
+export default Category;
